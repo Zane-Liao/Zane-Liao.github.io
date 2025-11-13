d795d1ebbc5fe047cf05b89163d705167e27ff37
Zane-Liao <lzq666amn@gmail.com>
2025-11-13 03:06:44 +0000

    Deploying to gh-pages from @ Zane-Liao/Zane-Liao.github.io@632a25ee8b281c029c36e98240e3eaf4bf97ca4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -83,17 +83,6 @@ ninja.data = [{
             window.location.href = "/blog/2025/plotly/";
           
         },
-      },{id: "post-a-post-with-image-galleries",
-        
-          title: "a post with image galleries",
-        
-        description: "this is what included image galleries could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/photo-gallery/";
-          
-        },
       },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
